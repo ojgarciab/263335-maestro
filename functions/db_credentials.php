896b12f67ac156d00ac257f996c57085649455f7
Oscar García <oscar.github@linaresdigital.com>
2019-05-14 19:33:00 +0200

    Eliminado cierre de PHP y cambios en credenciales

--- functions/db_credentials.php
+++ functions/db_credentials.php
@@ -1,10 +1,6 @@
 <?php 
-
 define("DB_SERVER", "localhost");
-define("DB_USER", "mrkondor");
-define("DB_PASS", "administrador");
+define("DB_USER", "maestro");
+define("DB_PASS", "Mvhjy67en49xcas0");
 define("DB_NAME", "maestro");
-
-
-?>
 
